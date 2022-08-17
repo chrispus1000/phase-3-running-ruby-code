a6151944033912125f036a38dfa314c0d201d63c
chrispus1000 <minrevac@gmail.com>
2022-08-18 00:55:26 +0300

    commit tree 9f9e624a25d2f762a1e1e74eb6369adbe2399d65

---
+++ app.rb
@@ -0,0 +1,14 @@
+puts "Hello World"
+puts "Hello World!"
+puts "Hello World!"
+
+print "Hello World!"
+print "Hello World!"
+
+puts [1, 2, 3]
+
+p [1, 2, 3]
+
+pp [{ id: 1, hello: "world"}, { id: 2, hello: "Ruby"}].inspect
+
+print "Pass this test, please."
